12a1c928e5daaa1f0bdde81e7caa1af24c2eb74d
José Carlos Palma <palmahn@gmail.com>
2021-02-15 00:14:45 -0600

    Pipes no cómunes i18nSelected, i18Plurar, JSon, KeyValue y Async.

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-
+import { RippleModule } from 'primeng/ripple';
 @NgModule({
     exports: [
+        BadgeModule,
         ButtonModule,
         CardModule,
-        BadgeModule,
-        MenubarModule
+        FieldsetModule,
+        MenubarModule,
+        RippleModule
     ]
 })
 export class PrimeNgModule { }
--- src/app/sales/pages/uncommon/uncommon.component.ts
+++ src/app/sales/pages/uncommon/uncommon.component.ts
@@ -1,16 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-uncommon',
-  templateUrl: './uncommon.component.html',
-  styles: [
-  ]
+    selector: 'app-uncommon',
+    templateUrl: './uncommon.component.html',
+    styles: []
 })
-export class UncommonComponent implements OnInit {
+export class UncommonComponent {
 
-  constructor() { }
+    name = 'José';
+    gender = 'm';
+    mapInvitation = {
+        m: 'invitarlo',
+        f: 'invitarla'
+    };
 
-  ngOnInit(): void {
-  }
+    clients: string[] = ['José', 'Carlos', 'María', 'Tulio', 'Gabriela'];
 
+    mapClient = {
+        '=0': 'ningún cliente esperando',
+        1: 'un cliente esperando',
+        2: 'dos clientes esperando',
+        other: '# clientes esperando'
+    };
+
+    person = {
+        name: 'José Carlos Palma',
+        age: 38,
+        address: 'Res. Santa Cruz'
+    };
+
+    heros = [
+        { name: 'Superman', power: 'Super fuerza' },
+        { name: 'Batman', power: 'Dinero' },
+        { name: 'Aquaman', power: 'Acuático' },
+    ];
+
+    emmitting = false;
+
+    observable: Observable<number> = interval(2000)
+        .pipe(
+            tap(n => this.emmitting = true)
+        );
+
+    promesa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promesa terminada!');
+        }, 3000);
+    });
+
+    delete(): void {
+        this.clients.pop();
+    }
+
+    changePerson(): void {
+        if (this.name === 'José') {
+            this.name = 'María';
+            this.gender = 'f';
+        } else {
+            this.name = 'José';
+            this.gender = 'm';
+        }
+    }
 }
