949ae734eff990d7c0283ce7c3c9667ef105874a
José Carlos Palma <palmahn@gmail.com>
2021-02-14 21:51:48 -0600

    Utilización de los pipe Decimal y Percent.

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numbers',
-  templateUrl: './numbers.component.html',
-  styles: [
-  ]
+    selector: 'app-numbers',
+    templateUrl: './numbers.component.html',
+    styles: [
+    ]
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    sales = 13442345.24538;
+    margen = 0.045563;
 
 }
